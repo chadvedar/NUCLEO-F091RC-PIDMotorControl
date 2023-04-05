fa78c053fea04fcdb8c968a3a06d412e5f93baa7
chadvedar <lee.jira.natee@gmail.com>
2023-04-05 06:29:40 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mbed.h"
+#include "RotaryEncoder.hpp"
+#include "PIDmotor.hpp"
+
+PIDmotor pidMotor(D5, D4);
+RotaryEncoder enc(D7, D8);
+
+float setpoint = 40.0;
+
+int main() {
+    
+    pidMotor.set_pid_gain(20.0, 0.001, 0.005, 0.0);
+
+    enc.init();
+    pidMotor.init();
+
+    enc.start();
+    pidMotor.start();
+
+    while(1) {
+        pidMotor.update_current_speed(enc.rpm);
+        pidMotor.set_target_speed(setpoint);
+
+        printf("%f, %f\n",setpoint, enc.rpm);
+        ThisThread::sleep_for(1ms);
+    }
+    
+}
