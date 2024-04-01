3a7afdfc501c7ae2c7239a234e2c91eae1e99d9e
chadvedar <lee.jira.natee@gmail.com>
2024-04-01 05:03:25 +0000

    latest ver.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,24 @@
 #include "mbed.h"
-#include "RotaryEncoder.hpp"
-#include "PIDmotor.hpp"
+#include "MotorControl.hpp"
 
-PIDmotor pidMotor(D5, D4);
-RotaryEncoder enc(D7, D8);
+MotorCtrl motor(D4, D5, D6, D7);
 
 float setpoint = 40.0;
 
 int main() {
     
-    pidMotor.set_pid_gain(20.0, 0.001, 0.005, 0.0);
+    motor.motor.set_pid_gain(20.0, 0.001, 0.005, 0.0);
 
-    enc.encoder_resolution = 0.000625;
-    enc.init();
-    pidMotor.init();
-
-    enc.start();
-    pidMotor.start();
+    motor.set_encoder_resolution(0.0007633);
+    motor.init();
+    motor.start();
 
     while(1) {
-        pidMotor.update_current_speed(enc.rpm);
-        pidMotor.set_target_speed(setpoint);
+        motor.update();
+        motor.setSpeed(setpoint);
 
-        printf("%f, %f\n",setpoint, enc.rpm);
-        ThisThread::sleep_for(1ms);
+        printf("%f, %f\n",setpoint, motor.getMotorSpeed());
+        wait_us(1000);
     }
     
 }
